d71b5bcdc45cf6aefb9a9982b1d4d39da7edd9f9
BAIBASH1 <gazinyr2014@mail.ru>
2023-10-09 03:33:25 +0300

    unpacked the passed parameters

--- publish_xkcd.py
+++ publish_xkcd.py
@@ -33,19 +33,26 @@ def download_new_xkcd(num_xkcd):
         file.write(xkcd_text)
 
 
-def get_url_for_upload(main_params):
+def get_url_for_upload(vk_api_access_token, vk_api_version):
     api_url_upload = urljoin(BASE_URL_VKAPI, 'photos.getWallUploadServer')
-    response_upload = requests.get(api_url_upload, params=main_params)
+    params = {
+        'access_token': vk_api_access_token,
+        'v': vk_api_version
+    }
+    response_upload = requests.get(api_url_upload, params=params)
     response_upload.raise_for_status()
     url_for_download = response_upload.json()['response']['upload_url']
     return url_for_download
 
 
-def save_image(main_params, params_for_save):
+def save_image(vk_api_access_token, vk_api_version, server, photo, response_hash):
     url_for_save = urljoin(BASE_URL_VKAPI, 'photos.saveWallPhoto')
     params = {
-        **main_params,
-        **params_for_save
+        'access_token': vk_api_access_token,
+        'v': vk_api_version,
+        'server': server,
+        'photo': photo,
+        'hash': response_hash
     }
     response_save = requests.get(url_for_save, params=params)
     response_save.raise_for_status()
@@ -55,28 +62,33 @@ def save_image(main_params, params_for_save):
     return image_id, owner_id
 
 
-def upload_and_save_photo(main_params, url_for_download, group_id, num_xkcd):
+def upload_and_save_photo(vk_api_access_token, vk_api_version, url_for_download, group_id, num_xkcd):
     with open(f'Files/image{num_xkcd}.jpg', 'rb') as file:
         files_upload = {
             'photo': file
         }
         params_for_upload = {
-            **main_params,
+            'access_token': vk_api_access_token,
+            'v': vk_api_version,
             'group_id': group_id
         }
         response_upload = requests.post(url_for_download, params=params_for_upload, files=files_upload)
         response_upload.raise_for_status()
     params_for_save = response_upload.json()
-    image_id, owner_id = save_image(main_params, params_for_save)
+    server = params_for_save['server']
+    photo = params_for_save['photo']
+    response_hash = params_for_save['hash']
+    image_id, owner_id = save_image(vk_api_access_token, vk_api_version, server, photo, response_hash)
     return image_id, owner_id
 
 
-def post_photo(main_params, group_id, image_id, owner_id, num_xkcd):
+def post_photo(vk_api_access_token, vk_api_version, group_id, image_id, owner_id, num_xkcd):
     url_for_post = urljoin(BASE_URL_VKAPI, 'wall.post')
     with open(f'Files/message{num_xkcd}.txt', 'r') as file:
         xkcd_text = file.read()
     params = {
-        **main_params,
+        'access_token': vk_api_access_token,
+        'v': vk_api_version,
         'owner_id': f'-{group_id}',
         'from_group': 1,
         'attachments': f'photo{owner_id}_{image_id}',
@@ -90,16 +102,14 @@ def post_photo(main_params, group_id, image_id, owner_id, num_xkcd):
 def main():
     load_dotenv()
     vk_group_id = os.environ['VK_GROUP_ID']
-    main_params = {
-        'access_token': os.environ['VK_API_ACCESS_TOKEN'],
-        'v': '5.150',
-    }
+    vk_api_access_token = os.environ['VK_API_ACCESS_TOKEN']
+    vk_api_version = '5.150'
     amount_xckd = get_amount_xckd()
     num_xkcd = random.randint(1, amount_xckd)
     download_new_xkcd(num_xkcd)
-    url_for_upload = get_url_for_upload(main_params)
-    image_id, owner_id = upload_and_save_photo(main_params, url_for_upload, vk_group_id, num_xkcd)
-    post_photo(main_params, vk_group_id, image_id, owner_id, num_xkcd)
+    url_for_upload = get_url_for_upload(vk_api_access_token, vk_api_version)
+    image_id, owner_id = upload_and_save_photo(vk_api_access_token, vk_api_version, url_for_upload, vk_group_id, num_xkcd)
+    post_photo(vk_api_access_token, vk_api_version, vk_group_id, image_id, owner_id, num_xkcd)
 
 
 if __name__ == '__main__':
